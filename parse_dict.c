6ce6f7ae678fe2206b50b02257a3dd9ecd41259a
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-05-24 11:59:48 +0700

    test_parse

--- parse_dict.c
+++ parse_dict.c
@@ -19,16 +19,26 @@ int	count_lines(char *file_path)
 	return (lines);
 }
 
-static int	get_val_len(char *str)
+static char	*get_val(char *str)
 {
-	int	i;
+	int		i;
+	int		len;
+	char	*val;
 
 	i = 0;
-	while (str[i] == ' ' || (str[i] < 9 || str[i] > 13))
-		str++;
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	return (i);
+	len = 0;
+	while (str[i + len] != '\0' && str[i + len] != '\n')
+		len++;
+	val = malloc(sizeof(char) * (len + 1));
+	if (!val)
+		return (NULL);
+	len = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		val[len++] = str[i++];
+	val[len] = '\0';
+	return (val);
 }
 
 int	parse_line(char *line, t_dict *entry)
@@ -37,7 +47,7 @@ int	parse_line(char *line, t_dict *entry)
 	int	k;
 
 	i = 0;
-	while (line[i] < '0' || line[i] > '9')
+	while (line[i] >= '0' && line[i] <= '9')
 		i++;
 	if (i == 0)
 		return (0);
@@ -48,9 +58,60 @@ int	parse_line(char *line, t_dict *entry)
 	while (++k < i)
 		entry->key[k] = line[k];
 	entry->key[k] = '\0';
-	while (line[i] == ' ' || (line[i] < 9 || line[i] > 13))
+	while (line[i] == ' ' || (line[i] >= 9 && line[i] <= 13))
 		i++;
 	if (line[i] != ':')
 		return (0);
+	entry->value = get_val(&line[i + 1]);
+	if (!entry->value)
+		return (0);
 	return (1);
+}
+
+static t_dict	*init_dict(char *file_path, int *size, int *fd)
+{
+	t_dict	*dict;
+
+	*size = count_lines(file_path);
+	if (*size <= 0)
+		return (NULL);
+	dict = malloc(sizeof(t_dict) * (*size + 1));
+	if (!dict)
+		return (NULL);
+	*fd = open(file_path, O_RDONLY);
+	if (*fd < 0)
+	{
+		free(dict);
+		return (NULL);
+	}
+	return (dict);
+}
+
+t_dict	*parse_dict(char *file_path, int *size)
+{
+	int		fd;
+	char	line[4096];
+	int		i;
+	int		li;
+	t_dict	*dict;
+
+	dict = init_dict(file_path, size, &fd);
+	if (!dict)
+		return (NULL);
+	i = 0;
+	li = 0;
+	while (read(fd, &line[i], 1) > 0)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = '\0';
+			if (line[0] != '\0' && !parse_line(line, &dict[li++]))
+				return (NULL);
+			i = -1;
+		}
+		i++;
+	}
+	close(fd);
+	dict[li].key = NULL;
+	return (dict);
 }
